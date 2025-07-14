5f1a7291c27e80a8b4029d3039b1987bfdc6619e
Am <ilhamsuldani2402@gmail.com>
2025-07-14 19:14:02 +0800

    Update m-vless.sh

--- menu/m-vless.sh
+++ menu/m-vless.sh
@@ -74,7 +74,7 @@ if check_ip_and_get_info "$IP_VPS"; then
     fi
 else
     echo -e "${RED}❌ IP tidak terdaftar!${NC}"
-    echo -e "➥ Hubungi admin ${CYAN}「 ✦ @SHokageLegend ✦ 」${NC}"
+    echo -e "➥ Hubungi admin ${CYAN}「 ✦ @amganteng ✦ 」${NC}"
     exit 1
 fi
 
@@ -358,7 +358,7 @@ echo -e "$COLOR1 ${NC} ${WH}http://$domain:89/vless-$user.txt${NC}" | tee -a /et
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ${NC} ${WH}Expired Until   ${COLOR1}: ${WH}$exp" | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
-echo -e "$COLOR1 ${NC} ${WH}    • ROSI VPN STORE •    " | tee -a /etc/vless/akun/log-create-${user}.log
+echo -e "$COLOR1 ${NC} ${WH}    • AM STORE VPN •    " | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
 echo "" | tee -a /etc/vless/akun/log-create-${user}.log
 systemctl restart xray > /dev/null 2>&1
@@ -544,7 +544,7 @@ echo -e "$COLOR1 ${NC} ${WH}http://$domain:89/vless-$user.txt${NC}" | tee -a /et
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ${NC} ${WH}Expired Until   ${COLOR1}: ${WH}$timer Minutes" | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
-echo -e "$COLOR1 ${NC} ${WH}    • HOKAGE LEGEND STORE •    " | tee -a /etc/vless/akun/log-create-${user}.log
+echo -e "$COLOR1 ${NC} ${WH}    • AM STORE VPN •    " | tee -a /etc/vless/akun/log-create-${user}.log
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vless/akun/log-create-${user}.log
 echo "" | tee -a /etc/vless/akun/log-create-${user}.log
 systemctl restart xray > /dev/null 2>&1
@@ -1260,7 +1260,7 @@ echo -e " $COLOR1║ $NC                                                  ${NC} 
 echo -e " $COLOR1║ $NC ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}GO BACK${NC}          ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}RESTORE AKUN   ${NC}    $COLOR1║ $NC"
 echo -e " $COLOR1╚════════════════════════════════════════════════════╝${NC}"
 echo -e " $COLOR1╔═════════════════════════ ${WH}BY${NC} ${COLOR1}═══════════════════════╗ ${NC}"
-echo -e "  $COLOR1${NC}           ${WH}   • HOKAGE LEGEND STORE •                $COLOR1 $NC"
+echo -e "  $COLOR1${NC}           ${WH}   • SELMA MULIATI STORE •                $COLOR1 $NC"
 echo -e " $COLOR1╚════════════════════════════════════════════════════╝${NC}"
 echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
